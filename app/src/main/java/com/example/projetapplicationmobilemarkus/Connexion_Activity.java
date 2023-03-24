30daa52ca6acf5967bce6fa59ffb771aa8f1d289
Jade Morin <jade.morin2001@outlook.com>
2023-03-24 13:06:55 -0400

    UI REGISTER + LOGIN

--- app/src/main/java/com/example/projetapplicationmobilemarkus/Connexion_Activity.java
+++ app/src/main/java/com/example/projetapplicationmobilemarkus/Connexion_Activity.java
@@ -1,14 +1,10 @@
 package com.example.projetapplicationmobilemarkus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Connexion_Activity extends AppCompatActivity {
 
@@ -21,55 +17,67 @@ public class Connexion_Activity extends AppCompatActivity {
      */
 
     //DÉCLARATIONS
-    EditText ETNomUser, ETPwd;
-    TextView TVMessageErreur;
-    Button BTNConnexion;
+//    EditText ETNomUser, ETPwd;
+//    TextView TVMessageErreur;
+//    Button BTNConnexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
 
-        TVMessageErreur = findViewById(R.id.TVMessageErreurConnexion);
 
-        ETNomUser = findViewById(R.id.ETNomUtilisateurConnexion);
-        ETPwd = findViewById(R.id.ETPwdConnexion);
+        //Appeler une activité à démarrer = MAIN ACTIVITY
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Connexion_Activity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
 
-        BTNConnexion = findViewById(R.id.BTNConnexion);
+
+
+//        TVMessageErreur = findViewById(R.id.TVMessageErreurConnexion);
+//
+//        ETNomUser = findViewById(R.id.ETNomUtilisateurConnexion);
+//        ETPwd = findViewById(R.id.ETPwdConnexion);
+//
+//        BTNConnexion = findViewById(R.id.BTNConnexion);
     }
 
     //--------------------------------------------------------------------------------------
     //------------------ GestionClikConnexion() ------------------
-    public void GestionClickConnexion(View view)
-    {
-        if(view.getId() == R.id.BTNConnexion)
-        {
-            //VÉRIFICATIONS DES EDITTEXT
-            //USER VIDE ET/OU MOT DE PASSE
-            if(ETPwd.getText().toString().equals("") ||
-                    ETNomUser.getText().toString().equals(""))
-            {
-                TVMessageErreur.setText("*Veuillez remplir tous les champs");
-            }
-
-            //MATCH ?
-            else if(!ETNomUser.getText().toString().equals("admin") ||
-                    !ETPwd.getText().toString().equals("admin"))
-            {
-                TVMessageErreur.setText("*Nom d'utilisateur ou mot de passe invalide!");
-            }
-
-            //OK
-            else if (ETNomUser.getText().toString().equals("admin") &&
-                    ETPwd.getText().toString().equals("admin"))
-            {
-                TVMessageErreur.setText("");
-
-                //Appeler une activité à démarrer = MAIN ACTIVITY
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }
-    }
+//    public void GestionClickConnexion(View view)
+//    {
+//        if(view.getId() == R.id.BTNConnexion)
+//        {
+//            //VÉRIFICATIONS DES EDITTEXT
+//            //USER VIDE ET/OU MOT DE PASSE
+//            if(ETPwd.getText().toString().equals("") ||
+//                    ETNomUser.getText().toString().equals(""))
+//            {
+//                TVMessageErreur.setText("*Veuillez remplir tous les champs");
+//            }
+//
+//            //MATCH ?
+//            else if(!ETNomUser.getText().toString().equals("admin") ||
+//                    !ETPwd.getText().toString().equals("admin"))
+//            {
+//                TVMessageErreur.setText("*Nom d'utilisateur ou mot de passe invalide!");
+//            }
+//
+//            //OK
+//            else if (ETNomUser.getText().toString().equals("admin") &&
+//                    ETPwd.getText().toString().equals("admin"))
+//            {
+//                TVMessageErreur.setText("");
+//
+//                //Appeler une activité à démarrer = MAIN ACTIVITY
+//                Intent intent = new Intent(this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+        //}
+//    }
 }
